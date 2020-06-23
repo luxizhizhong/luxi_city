926ab2145720d5c7263573b0e8cadb866bf84750
d1y <chenhonzhou@gmail.com>
2020-06-24 00:45:05 +0800

    完善推荐商家

--- lib/views/app/home.dart
+++ lib/views/app/home.dart
@@ -9,6 +9,7 @@ import 'package:luxi_city/api/luxi.dart';
 import 'package:luxi_city/config/const.dart';
 import 'package:list_ext/list_ext.dart';
 import 'package:luxi_city/widget/widget.dart';
+import 'package:rating_bar/rating_bar.dart';
 
 class AppHomeView extends StatefulWidget {
   @override
@@ -27,12 +28,9 @@ class AppHomeViewState extends State<AppHomeView> {
 
   void getStoreLists() async {
     List _list = await LuxiCityApi.getStores(page: _page);
-    int temp = _stores.length;
-    print("当前长度: $temp");
+    List _newList = _stores.copyWithAll(_list);
     setState(() {
-      List _newList = _stores.copyWithAll(_list);
-      print("新的长度: ${ _newList.length }");
-      print("测试长度: ${ _stores.length }");
+      _stores = _newList;
     });
   }
 
@@ -107,28 +105,174 @@ class AppHomeViewState extends State<AppHomeView> {
             title: "推荐商家",
             barColor: Colors.yellow
           ),
-          _storeItem()
+          Column(
+            children: _stores.map((item) {
+              return _storeItem(item);
+            }).toList(),
+          )
         ],
       ),
     );
   }
 
-  Widget _storeItem() {
-    return Column(
-      children: <Widget>[
-        Flex(
-          direction: Axis.horizontal,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
+  Widget _storeItem(Map item) {
+    // String _tips = "您当前的位置不在商家配送范围内";
 
-          ],
-        ),
-        Text(
-          "个个都是人才",
-          textAlign: TextAlign.center,
+    /// 这里是真的恶心, 忽略这里的代码
+    // int _py = item['shipping_cut'];
+    // var Fuck = item['shipping_fee'];
+    // bool _pyFlag = _py == Fuck;
+    // var _resPy = _py.toStringAsFixed(2); /// kill the line
+    /// 求求后台不要写前端行不行..
+    
+    return Container(
+      margin: EdgeInsets.only(
+        top: 2.4,
+        bottom: 2.4
+      ),
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            width: 1,
+            color: Colors.grey[300]
+          )
         )
-      ],
+      ),
+      child: Column(
+        children: <Widget>[
+          Flex(
+            direction: Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(8),
+                // height: 100,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Image.network(
+                        item['picture'],
+                        width: 62,
+                        height: 62,
+                        fit:BoxFit.fill,
+                        errorBuilder: (BuildContext context, Object exception, StackTrace stackTrace) {
+                          return Container(
+                            width: 62,
+                            height: 62,
+                            decoration: BoxDecoration(
+                              color: Color.fromRGBO(255, 0, 0, .4),
+                            ),
+                            // child: Center(
+                            //   child: Text("加载失败"),
+                            // ),
+                          );
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 5.2,
+                        left: 12
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(
+                              bottom: 4
+                            ),
+                            child: Text(
+                              item['name'],
+                              style: TextStyle(
+                                color: Colors.black87
+                              )
+                            ),
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              RatingBar.readOnly(
+                                filledColor: Colors.yellow,
+                                initialRating: 3.5,
+                                isHalfAllowed: true,
+                                halfFilledIcon: Icons.star_half,
+                                filledIcon: Icons.star,
+                                emptyIcon: Icons.star_border,
+                                size: 22,
+                              ),
+                              Text("销量:${ item['goods_count'] }")
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: 4,
+                              bottom: 4
+                            ),
+                            /// 这里不知道为什么要这么设计
+                            /// 以前的单价 - 当前的单价(应该是这样的)
+                            /// shipping_fee: 3 - shipping_cut: "3"
+                            /// 猜测就是为了拿到浮点数..
+                            /// 我通过看混淆后的代码还是没看懂这里的逻辑
+                            /// 有一点点无语 - 陈某 <chenhonzhou@gmail.com>
+                            child: RichText(
+                              text: TextSpan(
+                                children: <InlineSpan>[
+                                  TextSpan(
+                                    text: "起送￥${ item['least_money'] }",
+                                    style: TextStyle(
+                                      color: Colors.black54
+                                    ),
+                                  ),
+                                  WidgetSpan(
+                                    child: Container(
+                                      margin: EdgeInsets.only(
+                                        right: 4,
+                                        left: 4
+                                      ),
+                                      child: Text("|"),
+                                    ),
+                                    style: TextStyle(
+                                      color: Colors.black54
+                                    )
+                                  ),
+                                  TextSpan(
+                                    text:  false ? "免配送费" : '配送费￥${ item['shipping_fee'] }',
+                                    style: TextStyle(
+                                      color: Colors.black54
+                                    ),
+                                  ),
+                                ]
+                              ),
+                              
+                            ),
+                          ),
+                        ]
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Text(
+                "${ item['transit_time'] }分钟"
+              ),
+            ],
+          ),
+          // TODO, 不知道这里的判断条件是什么
+          // Text(
+          //   _tips,
+          //   textAlign: TextAlign.center,
+          //   style: TextStyle(
+          //     fontWeight: FontWeight.bold,
+          //     fontSize: 12,
+          //     color: Colors.red
+          //   )
+          // )
+        ],
+      )
     );
   }
 
